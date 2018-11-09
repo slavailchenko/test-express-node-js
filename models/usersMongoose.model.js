3c98fd175507ec5d2e5361fd1d33764aff7241a3
slavailchenko35 <slavailchenko35@gmail.com>
2018-11-09 11:48:15 +0200

    test mongoose v2.25 valid email

--- models/usersMongoose.model.js
+++ models/usersMongoose.model.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 const { ObjectId } = Schema.Types;
 
+const validateEmail = (email) => {
+    let r = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return r.test(email)
+};
+
 const userSchema = new Schema({
     firstName: {
         type: String,
@@ -16,7 +21,10 @@ const userSchema = new Schema({
     email: {
         type: String,        
         unique: true,
-        required: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+        'Please fill a valid email address'],
         trim: true
     },
     phone: {
